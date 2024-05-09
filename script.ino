00f1838eb6d4523b38fbeffb2257cd51ea5f6684
Sohag <128306527+ahadulsohag@users.noreply.github.com>
2024-05-09 21:40:19 +0600

    Update script.ino

--- script.ino
+++ script.ino
@@ -1,6 +1,6 @@
 #include<Arduino.h>
-int trig = 9, trig1 = 5, echo = 10, echo1 = 6, dis, b = 7;
-long dura;
+int trig = 9, trig1 = 5, echo = 10, echo1 = 6, dis, b = 7, dis1;
+long dura, dura1;
 void setup() {
   Serial.begin(9600);
   pinMode(echo, INPUT);
@@ -18,10 +18,17 @@ void loop() {
   digitalWrite(trig, LOW);
   dura = pulseIn(echo, HIGH);
   dis = dura * 0.034/2;
+  digitalWrite(trig1, LOW);
+  delayMicroseconds(10);
+  digitalWrite(trig1, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig1, LOW);
+  dura1 = pulseIn(echo1, HIGH);
+  dis1 = dura1 * 0.034/2;
   Serial.print("Distance: ");
-  Serial.println(dis);
+  Serial.println(dis, dis1);
   delay(500);
-  if(dis<=1100){
+  if(dis<=1100 && dis<=1100){
     digitalWrite(b,HIGH);
   }
 }
